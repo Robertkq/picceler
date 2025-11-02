04529e17f8edf527b0b0e9b0fd0ed617077add08
Tamas Andrei-Stefan <andrei-stefan.tamas@intel.com>
2025-11-02 21:26:20 +0200

    Early 'working' version

---
+++ src/picceler_const_ops_to_llvm_ir_pass.cpp
@@ -0,0 +1,101 @@
+#include <spdlog/spdlog.h>
+
+#include "passes.h"
+#include "ops.h"
+#include "types.h"
+
+#include <mlir/IR/PatternMatch.h>
+#include <mlir/Transforms/GreedyPatternRewriteDriver.h>
+#include <mlir/Dialect/Func/IR/FuncOps.h>
+#include <mlir/IR/SymbolTable.h>
+#include <mlir/Dialect/LLVMIR/LLVMDialect.h>
+
+#include <llvm/ADT/StringExtras.h>
+
+namespace picceler {
+
+struct StringConstToCall : public mlir::OpRewritePattern<StringConstOp> {
+  using OpRewritePattern::OpRewritePattern;
+
+  mlir::LogicalResult matchAndRewrite(
+      StringConstOp op, mlir::PatternRewriter &rewriter) const override {
+
+    auto module = op->getParentOfType<mlir::ModuleOp>();
+    if (!module)
+      return rewriter.notifyMatchFailure(op, "not inside a ModuleOp");
+
+    auto loc = op.getLoc();
+    llvm::StringRef s = op.getValue();
+    spdlog::info("StringConst: {}", s);
+
+    // Convert to null-terminated bytes
+    std::string bytes(s.begin(), s.end());
+    bytes.push_back('\0');
+
+    // Create a unique name for the global
+    llvm::SmallString<64> name;
+    llvm::hash_code hc = llvm::hash_value(s);
+    name += "__picceler_str_";
+    name += llvm::utohexstr((uint64_t)hc);
+
+    // LLVM types
+    auto *ctx = rewriter.getContext();
+    auto i8Type = mlir::IntegerType::get(ctx, 8);
+    auto arrayType = mlir::LLVM::LLVMArrayType::get(i8Type, bytes.size());
+    auto ptrType = mlir::LLVM::LLVMPointerType::get(ctx);
+
+    // Insert global at the start of the module
+    mlir::OpBuilder::InsertionGuard guard(rewriter);
+    rewriter.setInsertionPointToStart(module.getBody());
+
+    // Create the global string constant
+    auto global = rewriter.create<mlir::LLVM::GlobalOp>(
+        loc, arrayType, /*isConstant=*/true,
+        mlir::LLVM::Linkage::Internal, name,
+        rewriter.getStringAttr(bytes));
+
+    // Create the address-of op
+    mlir::Value globalPtr =
+        rewriter.create<mlir::LLVM::AddressOfOp>(loc,
+        mlir::LLVM::LLVMPointerType::get(ctx), global.getSymName());
+
+
+    // Compute GEP to first element (i8* pointer)
+    mlir::Value zero = rewriter.create<mlir::LLVM::ConstantOp>(
+        loc, rewriter.getI64Type(), rewriter.getI64IntegerAttr(0));
+
+    mlir::Value elementPtr = rewriter.create<mlir::LLVM::GEPOp>(
+        loc, ptrType, arrayType, globalPtr, mlir::ValueRange{zero, zero});
+
+    // Replace the original StringConstOp with this i8* value
+    rewriter.replaceOp(op, elementPtr);
+    return mlir::success();
+  }
+};
+
+void PiccelerConstOpsToLLVMIRPass::runOnOperation() {
+  spdlog::trace("Running PiccelerConstOpsToLLVMIRPass");
+  mlir::ModuleOp module = getOperation();
+
+  mlir::RewritePatternSet patterns(&getContext());
+
+  patterns.add<StringConstToCall>(&getContext());
+
+  if (mlir::failed(mlir::applyPatternsGreedily(module, std::move(patterns)))) {
+    signalPassFailure();
+  }
+}
+
+mlir::StringRef PiccelerConstOpsToLLVMIRPass::getArgument() const {
+  return "picceler-const-ops-to-llvm";
+}
+
+mlir::StringRef PiccelerConstOpsToLLVMIRPass::getDescription() const {
+  return "Convert Picceler const ops to LLVM IR ops";
+}
+
+void PiccelerConstOpsToLLVMIRPass::registerPass() {
+  mlir::PassRegistration<PiccelerConstOpsToLLVMIRPass>();
+}
+
+} // namespace picceler
