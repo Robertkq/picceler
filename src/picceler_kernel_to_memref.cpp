340bf56cdfccb8c3e5022510d0263ae679cd02a3
Robertkq <robertvuia06@gmail.com>
2026-01-14 20:58:26 +0200

    Start on kernel conversion pass

---
+++ src/picceler_kernel_to_memref.cpp
@@ -0,0 +1,32 @@
+#include "passes.h"
+
+#include "spdlog/spdlog.h"
+#include "mlir/Dialect/Arith/IR/Arith.h"
+#include "mlir/Dialect/LLVMIR/LLVMDialect.h"
+#include "mlir/Dialect/Func/IR/FuncOps.h"
+#include "mlir/Dialect/MemRef/IR/MemRef.h"
+#include "mlir/Transforms/DialectConversion.h"
+#include "mlir/IR/Builders.h"
+#include "mlir/Pass/Pass.h"
+#include "mlir/IR/BuiltinOps.h"
+
+#include "ops.h"
+#include "types.h"
+
+namespace picceler {
+
+struct KernelToMemref : mlir::OpConversionPattern<KernelConstOp> {
+
+  mlir::LogicalResult matchAndRewrite(KernelConstOp op, KernelConstOpAdaptor adaptor,
+                                      mlir::ConversionPatternRewriter rewriter) {}
+};
+
+void PiccelerKernelToMemrefPass::runOnOperation() {
+  mlir::ModuleOp module = getOperation();
+  mlir::MLIRContext *ctx = &getContext();
+  mlir::Location loc = module.getLoc();
+
+  mlir::RewritePatternSet patterns(ctx);
+  patterns.add<KernelToMemref>(ctx);
+}
+} // namespace picceler
