c300eef54b2305bf620512cd265572f876b6c610
Emi <94977236+emdevv@users.noreply.github.com>
2026-05-10 21:50:10 +0300

    moved implementations to .cpp (#38)

---
+++ src/image_access_helper.cpp
@@ -0,0 +1,44 @@
+#include "image_access_helper.h"
+
+namespace picceler {
+ImageAccessHelper::ImageAccessHelper(mlir::Value ptr, mlir::OpBuilder &b, mlir::Location l)
+    : structPtr(ptr), builder(b), loc(l) {}
+
+mlir::Type ImageAccessHelper::getImageStructType(mlir::MLIRContext *ctx) {
+  mlir::Type i32 = mlir::IntegerType::get(ctx, 32);
+  mlir::Type ptr = mlir::LLVM::LLVMPointerType::get(ctx);
+  return mlir::LLVM::LLVMStructType::getLiteral(ctx, {i32, i32, ptr});
+}
+
+mlir::Value ImageAccessHelper::getFieldAddr(int32_t index) {
+  auto structType = getImageStructType(builder.getContext());
+  auto ptrType = mlir::LLVM::LLVMPointerType::get(builder.getContext());
+
+  // We use GEPArg to tell MLIR these are static constant indices.
+  // {0, index} means: start at base pointer, go to field 'index'.
+  llvm::SmallVector<mlir::LLVM::GEPArg> indices;
+  indices.push_back(0);
+  indices.push_back(index);
+
+  return builder.create<mlir::LLVM::GEPOp>(loc,
+                                           ptrType,    // Result is a pointer to the field
+                                           structType, // The layout we are indexing into
+                                           structPtr,  // The base opaque pointer
+                                           indices);
+}
+
+mlir::Value ImageAccessHelper::getWidth() {
+  mlir::Type i32 = mlir::IntegerType::get(builder.getContext(), 32);
+  return builder.create<mlir::LLVM::LoadOp>(loc, i32, getFieldAddr(0));
+}
+
+mlir::Value ImageAccessHelper::getHeight() {
+  mlir::Type i32 = mlir::IntegerType::get(builder.getContext(), 32);
+  return builder.create<mlir::LLVM::LoadOp>(loc, i32, getFieldAddr(1));
+}
+
+mlir::Value ImageAccessHelper::getDataPtr() {
+  mlir::Type ptrType = mlir::LLVM::LLVMPointerType::get(builder.getContext());
+  return builder.create<mlir::LLVM::LoadOp>(loc, ptrType, getFieldAddr(2));
+}
+} // namespace picceler
